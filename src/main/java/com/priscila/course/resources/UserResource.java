192d564006d4541c315c06f86610c393749bfb83
wantoli <wadeoliveira81@gmail.com>
2020-05-06 21:55:01 -0300

    User entity and resource

---
+++ src/main/java/com/priscila/course/resources/UserResource.java
@@ -0,0 +1,22 @@
+package com.priscila.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.priscila.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+
+		User u = new User(1L, "pri", "pri@pri.com", "99999", "12345");		
+		return ResponseEntity.ok().body(u);
+		
+	}
+
+}
