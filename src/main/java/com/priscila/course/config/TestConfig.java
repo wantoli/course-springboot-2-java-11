83b106a216b4a17f2a963ef4e99f4882eb8dccdd
wantoli <wadeoliveira81@gmail.com>
2020-05-08 21:54:39 -0300

    Payment, one-to-one association

--- src/main/java/com/priscila/course/config/TestConfig.java
+++ src/main/java/com/priscila/course/config/TestConfig.java
@@ -12,6 +12,7 @@ import com.priscila.course.entitie.enums.OrderStatus;
 import com.priscila.course.entities.Category;
 import com.priscila.course.entities.Order;
 import com.priscila.course.entities.OrderItem;
+import com.priscila.course.entities.Payment;
 import com.priscila.course.entities.Product;
 import com.priscila.course.entities.User;
 import com.priscila.course.repositories.CategoryRepository;
@@ -83,6 +84,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	
 	} 
 	
