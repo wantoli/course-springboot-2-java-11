866a662c501c807a3e5f913fef814f895bfc3831
wantoli <wadeoliveira81@gmail.com>
2020-05-09 14:25:04 -0300

    User insert

--- src/main/java/com/priscila/course/resources/UserResource.java
+++ src/main/java/com/priscila/course/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.priscila.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.priscila.course.entities.User;
 import com.priscila.course.services.UserService;
@@ -36,4 +40,17 @@ public class UserResource {
 		
 	}
 
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		obj = service.insert(obj);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(obj);
+		
+		//return ResponseEntity.created(null).body(obj);
+		
+		//return ResponseEntity.ok().body(obj);
+	}
 }
